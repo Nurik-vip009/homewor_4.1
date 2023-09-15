a19d847de7e6c90d4f9f022db46d4d237bf56fe0
Nurik-vip009 <raimkulovnurislam095@gmail.com>
2023-09-15 06:41:35 +0600

    first commit

--- app/src/main/java/com/example/taskmanager/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/profile/ProfileFragment.kt
@@ -1,53 +1,25 @@
 package com.example.taskmanager.ui.profile
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.example.taskmanager.CircleCropTransformation
 import com.example.taskmanager.R
-import com.example.taskmanager.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
-    private var _binding: FragmentProfileBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+        }
+    }
 
-    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    ): View? {
 
-        val editText = requireView().findViewById<EditText>(R.id.editTextProfile)
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
-
-
-        val imageView = requireView().findViewById<ImageView>(R.id.imageViewProfile)
-        Glide.with(this)
-            .load(imageView)
-            .transform(CircleCropTransformation)
-            .into(imageView)
-
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
